1f52b965ace66da3414458719e3e225c2d0d0ff0
ronakjain1234 <ronakjain0919@utexas.edu>
2025-10-13 13:00:04 -0500

    Added updated

--- scripts/run_baseline.py
+++ scripts/run_baseline.py
@@ -1,4 +1,8 @@
 import argparse, yaml
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
 from adhocfl.config import Config, NetworkConfig, BatteryConfig, TrainingConfig, DatasetConfig, RunConfig
 from adhocfl.orchestrator import run
 
